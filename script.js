4f731615d4015cf0eafa8913ac1cda1e8fe0df96
Eduardo Lacerda <eduardolacerda@edu.unifil.br>
2022-05-12 20:41:06 -0300

    :sparkles: Incrementada adição de ordem dinâmica

--- script.js
+++ script.js
@@ -1,36 +1,128 @@
 (function() {
+    var ordemValue = null;
+    var generateButton = document.querySelector('#generateButton');
     var button = document.querySelector('#resultButton');
     var resultDiv = document.querySelector('#result');
+    var inputs = document.querySelector('.inputs');
+    var condition = document.querySelector('.condition');
+    var values = [];
+    var arrayValues = [];
+    var results = [];
+    var pivo;
+    var result;
 
-    button.addEventListener("click", function() {
-        resultDiv.innerHTML = returnResult();
+    generateButton.addEventListener("click", function() {
+        ordemValue = document.querySelector('.inputOrdem').value
+        let ordem = document.querySelector('.ordem');
+        let divHidden = document.querySelector('.hidden');
+        ordem.style.display = "none";
+        divHidden.removeAttribute("style");
+        gerateArray(ordemValue);
     });
 
-    function getElement(id) {
-        return document.querySelector(`#${id}`);
+    function gerateArray(ordem) {
+        for (let i = 1; i - 1 < ordem; i++) {
+            for (let j = 1; j - 1 < ordem; j++) {
+                let newDiv = document.createElement("div");
+                let newInput = document.createElement("input");
+                newDiv.setAttribute("class", "row");
+                newInput.setAttribute("type", "number");
+                newInput.setAttribute("class", "inputRow");
+                newInput.setAttribute("id", `l${i}c${j}`);
+                inputs.appendChild(newDiv);
+                newDiv.appendChild(newInput);
+            }
+            let br = document.createElement("br");
+            inputs.appendChild(br);
+        }
+
+        for (let k = 1; k - 1 < ordem; k++) {
+            let newDiv = document.createElement("div");
+            let newInput = document.createElement("input");
+            let br = document.createElement("br");
+            newDiv.setAttribute("class", "row");
+            newInput.setAttribute("type", "number");
+            newInput.setAttribute("class", "inputRow");
+            newInput.setAttribute("id", `r${k}`);
+            condition.appendChild(newDiv);
+            newDiv.appendChild(newInput);
+            condition.appendChild(br);
+        }
     }
 
-    function returnResult() {
-        if (getElement('l1c1').value == 0) {
-            return `<p class = "resultReturn" style="color:red;">Não foi possível realizar o cálclulo</p>`
+    button.addEventListener("click", function() {
+        resultDiv.innerHTML = returnResult(ordemValue);
+    });
+
+    function getValues(item) {
+        if (!item.getAttribute("id").includes("r")) {
+            values.push(item.value);
         }
-        let k = getElement('l2c1').value / getElement('l1c1').value;
-        let w = getElement('l3c1').value / getElement('l1c1').value;
+    }
 
-        let l2c2 = getElement('l2c2').value - k * getElement('l1c2').value;
-        let l2c3 = getElement('l2c3').value - k * getElement('l1c3').value;
-        let r2 = getElement('r2').value - k * getElement('r1').value;
-        let l3c2 = getElement('l3c2').value - w * getElement('l1c2').value;
+    function getResults(item) {
+        if (item.getAttribute("id").includes("r")) {
+            results.push(item.value);
+        }
+    }
 
-        let v = l3c2 / l2c2;
+    function setResultsInArray(item, index) {
+        item.push(results[index]);
+    }
 
-        let l3c3 = (getElement('l3c3').value - w * getElement('l1c3').value) - v * l2c3;
-        let r3 = (getElement('r3').value - w * getElement('r1').value) - v * r2;
+    function returnResult(ordem) {
+        let inputsValues = document.querySelectorAll('.inputRow');
+        inputsValues.forEach(getValues);
+        inputsValues.forEach(getResults);
+        console.log(ordem);
 
-        let z = r3 / l3c3;
-        let y = (r2 - l2c3 * z) / l2c2;
-        let x = (getElement('r1').value - getElement('l1c2').value * y - getElement('l1c3').value * z) / getElement('l1c1').value;
+        for (var i = 0; i < values.length; i = i + parseInt(ordem)) {
+            console.log(i);
+            arrayValues.push(values.slice(i, i + parseInt(ordem)));
+        }
 
-        return `<p class="resultReturn">[${x}, ${y}, ${z}]</p>`;
+        arrayValues.forEach(setResultsInArray);
+
+        return execute(arrayValues, ordem);
+    }
+
+    function execute(array, ordem) {
+        let valor = array;
+
+        var i = ordem;
+        i = parseInt(i);
+        j = i;
+        i = i - 1;
+        var x = [];
+
+        for (var l = 0; l < i; l++) {
+            pivo = valor[l][l];
+            for (var o = l + 1; o < i + 1; o++) {
+                var m = valor[o][l] / pivo;
+                if (valor[o][k] == 0) {} else {
+                    for (var k = 0; k < j + 1; k++) {
+                        val = (valor[o][k]) - (m * valor[l][k]);
+                        valor[o][k] = val.toFixed(5);
+                    }
+                }
+            }
+        }
+
+        for (y = i; y > -1; y--) {
+            result = valor[y][j];
+
+            for (m = 0; m < j; m++) {
+                if (m == y) {} else {
+                    result = result - valor[y][m];
+                }
+            }
+
+            x[y] = result / valor[y][y];
+            for (l = i; l > -1; l--) {
+                valor[l][y] = x[y] * valor[l][y];
+            }
+        }
+
+        return `<p class="resultReturn">[${x}]</p>`
     }
 })();
